28698098af72d36d410e771c05395a27955d8ab7
wlsheep <1507405355@qq.com>
2024-10-07 22:06:58 +0800

    create project

---
+++ src/main/java/org/sheep/sheepmall/SheepMallApplication.java
@@ -0,0 +1,13 @@
+package org.sheep.sheepmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheepMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheepMallApplication.class, args);
+    }
+
+}
